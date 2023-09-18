e066ee3d8b69e2493a76f00dd16772b026fa0330
Nate <nathan-dp@hotmail.co.uk>
2023-09-18 17:46:51 +0100

    Added All Platforms

--- scripts/hunterscroller.js
+++ scripts/hunterscroller.js
@@ -11,7 +11,7 @@ const c = canvas.getContext("2d");
 
 canvas.width = 1024;
 canvas.height = 576;
-const debug = new Debug(true);
+const debug = new Debug(false);
 const gravity = 0.49;
 
 // hitbox
@@ -168,6 +168,38 @@ function init() {
       y: 270,
       image: createImage(platformSmallTall),
     }),
+    new Platform({
+      x:
+        platformImage.width * 14 +
+        1200 -
+        2 +
+        platformImage.width -
+        platformSmallTallImage.width,
+      y: 270,
+      image: createImage(platformSmallTall),
+    }),
+    new Platform({
+      x:
+        platformImage.width * 15 +
+        1200 -
+        2 +
+        platformImage.width -
+        platformSmallTallImage.width,
+      y: 270,
+      image: createImage(platformSmallTall),
+    }),
+    new Platform({
+      x:
+        platformImage.width * 21 +
+        1200 -
+        2 +
+        platformImage.width -
+        platformSmallTallImage.width,
+      y: 270,
+      image: createImage(platformSmallTall),
+    }),
+
+    // flat platforms
 
     new Platform({
       x: -1,
@@ -194,8 +226,129 @@ function init() {
       y: 470,
       image: platformImage,
     }),
+
     new Platform({
       x: platformImage.width * 5 + 700 - 2,
+      y: 470,
+      image: platformImage,
+    }),
+    new Platform({
+      x: platformImage.width * 6 + 700 - 4,
+      y: 470,
+      image: platformImage,
+    }),
+    new Platform({
+      x: platformImage.width * 7 + 900 - 4,
+      y: 470,
+      image: platformImage,
+    }),
+    new Platform({
+      x: platformImage.width * 8 + 800 - 2,
+      y: 345,
+      image: platformImage,
+    }),
+    new Platform({
+      x: platformImage.width * 9 + 800 - 3,
+      y: 225,
+      image: platformImage,
+    }),
+    new Platform({
+      x: platformImage.width * 10 + 700 - 2,
+      y: 350,
+      image: platformImage,
+    }),
+    new Platform({
+      x: platformImage.width * 11 + 700 - 2,
+      y: 225,
+      image: platformImage,
+    }),
+    new Platform({
+      x: platformImage.width * 12 + 1200 - 2,
+      y: 470,
+      image: platformImage,
+    }),
+    new Platform({
+      x: platformImage.width * 13 + 1200 - 4,
+      y: 470,
+      image: platformImage,
+    }),
+    new Platform({
+      x: platformImage.width * 14 + 1200 - 6,
+      y: 470,
+      image: platformImage,
+    }),
+    new Platform({
+      x: platformImage.width * 15 + 1200 - 9,
+      y: 470,
+      image: platformImage,
+    }),
+    new Platform({
+      x: platformImage.width * 16 + 1200 - 11,
+      y: 470,
+      image: platformImage,
+    }),
+    new Platform({
+      x: platformImage.width * 17 + 1200 - 2,
+      y: 370,
+      image: platformImage,
+    }),
+    new Platform({
+      x: platformImage.width * 18 + 1200 - 2,
+      y: 270,
+      image: platformImage,
+    }),
+    new Platform({
+      x: platformImage.width * 19 + 1200 - 2,
+      y: 170,
+      image: platformImage,
+    }),
+    new Platform({
+      x: platformImage.width * 20 + 1200 - 2,
+      y: 70,
+      image: platformImage,
+    }),
+    new Platform({
+      x: platformImage.width * 21 + 1100 - 2,
+      y: 470,
+      image: platformImage,
+    }),
+    new Platform({
+      x: platformImage.width * 22 + 1100 - 4,
+      y: 470,
+      image: platformImage,
+    }),
+    new Platform({
+      x: platformImage.width * 22 + 1000 - 2,
+      y: 70,
+      image: platformImage,
+    }),
+    new Platform({
+      x: platformImage.width * 23 + 1250 - 2,
+      y: 170,
+      image: platformImage,
+    }),
+    new Platform({
+      x: platformImage.width * 24 + 1250 - 2,
+      y: 270,
+      image: platformImage,
+    }),
+    new Platform({
+      x: platformImage.width * 25 + 1250 - 2,
+      y: 370,
+      image: platformImage,
+    }),
+    new Platform({
+      x: platformImage.width * 26 + 1250 - 2,
+      y: 470,
+      image: platformImage,
+    }),
+    new Platform({
+      x: platformImage.width * 27 + 1250 - 4,
+      y: 470,
+      image: platformImage,
+    }),
+    new Platform({
+      x: platformImage.width * 28 + 1600 - 2,
       y: 470,
       image: platformImage,
     }),
@@ -216,7 +369,6 @@ function init() {
 
   scrollOffset = 0;
 }
-
 export function animate() {
   requestAnimationFrame(animate);
   c.fillStyle = "white";
@@ -299,8 +451,11 @@ export function animate() {
   }
 
   // win condition
-  if (scrollOffset > platformImage.width * 5 + 700 - 2) {
-    console.log("You Passed the Hunter Exam");
+  function restart() {
+    location.replace("https://dioality.glowfinger.com/");
+  }
+  if (scrollOffset > platformImage.width * 29 + 1100 - 2) {
+    alert("You Passed the Hunter Exam").onclick = restart();
   }
   // lose condition
   if (player.position.y > canvas.height) {
